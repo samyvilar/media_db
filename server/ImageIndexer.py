f62755b9f143e458f88860901ceda8469be31f80
Daniel Garfield <dbgarf@metaphorm.(none)>
2011-05-15 23:20:52 -0400

    major changes to ImageIndexer.py and VideoIndexer.py

---
+++ server/ImageIndexer.py
@@ -0,0 +1,129 @@
+__author__ = "Daniel Garfield"
+__date__="$May 14, 2011 5:36:00PM$"
+
+#this utilizes the Image class of PIL version 1.1.7, for list of dependencies see README
+import Image
+import ImageFilter
+import math
+
+class ImageIndexer(object):
+    "open images from file and produce histogram and edgemap index for the image"
+    def __init__(self, filename):
+        self.img = Image.open(filename)
+        self.width = self.img.size[0]
+        self.height = self.img.size[1]
+        self.filename = filename
+
+    def getImgInfo(self):
+        print "format = %s, size = %s, mode = %s" %(self.img.format, self.img.size, self.img.mode)
+
+    def makeGrayscale(self):
+        if self.img.mode != "L":
+            self.img = self.img.convert("L")
+   
+    def saveBands(self):
+        "splits the RGB image into 3 images, 1 per band, saves splits images to cwd"
+        if self.img.mode !="RGB":
+            return
+        data = self.img.getdata()
+        r_data = [(0, 0, 0)]*(self.width*self.height)
+        g_data = [(0, 0, 0)]*(self.width*self.height)
+        b_data = [(0, 0, 0)]*(self.width*self.height)
+        for i, value in enumerate(data):
+            r_data[i] = (value[0], 0, 0)
+            g_data[i] = (0, value[1], 0)
+            b_data[i] = (0, 0, value[2])
+        r_img = Image.new("RGB", self.img.size)
+        r_img.putdata(r_data)
+        r_img.save("red_band_of_"+self.filename, self.img.format)
+        g_img = Image.new("RGB", self.img.size)
+        g_img.putdata(g_data)
+        g_img.save("green_band_of_"+self.filename, self.img.format)
+        b_img = Image.new("RGB", self.img.size)
+        b_img.putdata(b_data)
+        b_img.save("blue_band_of_"+self.filename, self.img.format)
+        return "ok"
+
+    
+    def makeHistogram(self):
+        "returns a 16 bin histogram of the image, grayscale images return a single band hist, RGB returns a 3 band hist"
+        if self.img.mode =="L":
+            hist = {}
+            vals_vector = list(self.img.getdata())
+            for value in vals_vector:
+                key = value%16
+                try:
+                    hist[key] += 1
+                except KeyError:
+                    hist[key] = 1
+            return hist
+        elif self.img.mode =="RGB":
+            hist_r = {}
+            hist_g = {}
+            hist_b = {}
+            vals_vector = list(self.img.getdata())
+            for value in vals_vector:
+                key_r = value[0]%16
+                key_g = value[1]%16
+                key_b = value[2]%16
+                try:
+                    hist_r[key_r] += 1
+                    hist_g[key_g] += 1
+                    hist_b[key_b] += 1
+                except KeyError:
+                    hist_r[key_r] = 1
+                    hist_g[key_g] = 1
+                    hist_b[key_b] = 1
+            return (hist_r, hist_g, hist_b)
+        else:
+            return "something broke in makeHistogram"
+
+    def makeEdgeMap(self, thresh):
+        "creates an edge map with single threshold filtering. edges are at minimum stronger then thresh"
+        #first denoise image with Gaussian filter then Sharpen to avoid fat edges
+        temp_img = self.img.filter(ImageFilter.GaussianBlur)
+        temp_img = self.img.filter(ImageFilter.SHARPEN)
+        temp_img_data = list(temp_img.getdata())
+        width = temp_img.size[0]
+        height = temp_img.size[1]
+        edge_map = Image.new(temp_img.mode, temp_img.size)
+        edge_map_data = [0]*(width*height) #initialized to 0's, values will be altered as we go
+        #second step is to detect strongest edges using the [-1,-1,-1,-1,8,-1,-1,-1,-1] kernel
+        for rownum in range(1, height-1):
+            for i in range(1, width-1):
+                cur_mask = self.get_mask(temp_img_data, width, rownum, i)
+                if self.is_edgepoint(cur_mask, thresh): 
+                    edge_map_data[rownum*width + i] = cur_mask[4]
+                else:
+                    edge_map_data[rownum*width + i] = 0
+        #finish up
+        edge_map.putdata(edge_map_data)
+        edge_map.save("edgemap_of_"+self.filename, self.img.format)
+        return "ok"
+
+   
+    def get_mask(self, img_data, width, rownum, i):
+        "returns a flat 3x3 mask centered on pixel i"
+        mask = [0]*9
+        mask[0] = img_data[(rownum*width-1) + (i-1)]
+        mask[1] = img_data[(rownum*width-1) + i]
+        mask[2] = img_data[(rownum*width-1) + (i+1)]
+        mask[3] = img_data[(rownum*width) + (i-1)]
+        mask[4] = img_data[(rownum*width) + i]
+        mask[5] = img_data[(rownum*width) + (i+1)]
+        mask[6] = img_data[(rownum*width+1) + (i-1)]
+        mask[7] = img_data[(rownum*width+1) + i]
+        mask[8] = img_data[(rownum*width+1) + (i+1)]
+        return mask
+
+    def is_edgepoint(self, cur_mask, threshold):
+        "returns true if the central point of the mask is sufficiently brighter then its surroundings"
+        convolution = [-1, -1, -1, -1, 8, -1, -1, -1, -1]
+        sum = 0
+        for i in range(9):
+            sum +=  convolution[i]*cur_mask[i]
+        if sum > threshold:
+            return True
+        else:
+            return False
+    
---
+++ server/VideoIndexer.py
@@ -0,0 +1,103 @@
+import ImageIndexer
+import subprocess
+import os
+
+class VideoIndexer(object):
+    "class used for indexing videos by breaking a video into distinct scenes and making thumbnails of those scenes"
+    def __init__(self, filename):
+        self.filename = filename
+        self.vidinfo = self.getVidInfo()
+        self.length = self.getLength()
+
+    def getLength(self):
+        "returns the length (in seconds) of a video"
+        for field in self.vidinfo:
+            if "LENGTH" in field:
+                length = field.split("=")[1]
+        return length
+
+    def getVidInfo(self):
+        "calls the midentify.sh script to generate a list of video attributes"
+        proc_args = ["./midentify.sh", self.filename]
+        proc = subprocess.Popen(proc_args, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        result = proc.communicate()
+        vidinfo = result[0].split("\n")
+        return vidinfo
+
+    def getFrame(self, time):
+        "calls ffmpeg to generate a jpeg frame capture, then initialize an ImageIndexer on that frame and return it"
+        #example command: ffmpeg -ss 1000 -i input.avi -vframes 1 output.jpeg
+        file_title = self.filename.split(".")[0]
+        cwd = os.getcwd()
+        output_path = cwd+"/"+file_title+"_frames/"+file_title+"_time="+str(time)+".jpeg"
+        try:
+            #first of all, try to open the image to see if it already exists
+            img = ImageIndexer.ImageIndexer(output_path)
+            return img
+        except IOError:
+            #could not open image, so it doesn't exist and we need to create it
+            try:
+                #create directory if doesn't exist
+                os.mkdir(cwd+"/"+file_title+"_frames")
+            except OSError:
+                pass
+            #now make the frame
+            proc_args = ["ffmpeg", "-ss", str(time), "-i", self.filename, "-vframes", "1", "-y", output_path]
+            subprocess.check_call(proc_args, stderr=subprocess.PIPE)
+            img = ImageIndexer.ImageIndexer(output_path)
+            return img
+
+    def makeThumbnails(self, start_time, stop_time, number_of_thumbnails):
+        "makes thumbnails, evenly spaced between the time points given, inclusive of the endpoints, min thumbs is 3"
+        if number_of_thumbnails < 3:
+            thumbcount = 3
+        else:
+            thumbcount = number_of_thumbnails
+        interval = abs(float(stop_time) - float(start_time))/float(thumbcount-1)
+        for i in range(thumbcount):
+            time = start_time + i*interval
+            im = self.getFrame(time)
+        return "ok"
+ 
+    def dif_hist(self, img1, img2):
+        "computes the difference between img1 and img2 using the 1 norm, normalized between 0 and 100. uses PIL histogram for speed"
+        if img1.img.size != img2.img.size:
+            return "error in dif_hist2, img1.img.size != img2.img.size"
+        if img1.img.mode != img2.img.mode:
+            return "error in dif_hist2, img1.img.mode != img2.img.mode"
+        if img1.img.mode == "L":
+            max_sum = 2*img1.height*img1.width
+        elif img1.img.mode == "RGB":
+            max_sum = 3*2*img1.height*img1.width
+        hist1 = img1.img.histogram()
+        hist2 = img2.img.histogram()
+        if len(hist1) != len(hist2):
+            return "error in dif_hist2, len(hist1) != len(hist2)"
+        sum = 0
+        for i in range(len(hist1)):
+            sum += abs(hist1[i] - hist2[i])
+        normalized_sum = (float(sum)/float(max_sum))*100
+        return normalized_sum
+    
+    def sceneSearch(self, thresh):
+        "returns a list of the  approximate start time of scenes based on a threshold (normalized between 0 and 100, takes floating point args), note: as a side effect this function generates a fairly substantial amount of frame capture jpegs from the video clip"
+        #this is a bottom up implementation using loops
+        scene_change_times = []
+        window_size = 1.0
+        window_start = 0.0
+        window_end = window_start + window_size
+        while(window_end < float(self.length) ):
+            start_frame = self.getFrame(window_start)
+            end_frame = self.getFrame(window_end)
+            dif = self.dif_hist(start_frame, end_frame)
+            if dif < thresh: #window is in same scene, double window size and check again in next loop
+                window_size = window_size*2.0
+                window_end = window_start + window_size
+            else: #window found a scene boundary, record start of window and move window forward and reset for next loop
+                scene_change_times.append(window_start)
+                window_start = window_end
+                window_size = 1.0
+                window_end = window_start + window_size
+        return scene_change_times
+
+            
